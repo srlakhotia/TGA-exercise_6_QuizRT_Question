9c564190dd39131197328f753c153f78bfc5d40c
pghose <newton@newtonflash.com>
2018-01-17 14:58:26 +0530

    updated router

--- api/questions/questions.controller.js
+++ api/questions/questions.controller.js
@@ -1,8 +1,11 @@
-
-const getQuestions = function (requestObj){
+const questionService = require('./questions.service');
+const getQuestions = function (parameters, done) {
+    //questionService.getQuestions(parameters, done);
+    return done(null, parameters);
+    //return "question.controllers: You are here";
 
 };
 
 module.exports = {
-    getQuestions : getQuestions
-}
+    getQuestions
+};
--- api/questions/questions.router.js
+++ api/questions/questions.router.js
@@ -1,26 +1,38 @@
 const router = require('express').Router();
-const controller = require('./questions.router');
+const controller = require('./questions.controller');
 
 // topic, level of difficulty, number of questions
 router.get('/', function(req, res){
     console.log("routing here");
     try {
-        console.log(req);
-        let reqParams = req;
-        res.send("hello");
+        let reqParams = req.query;
+        let questionSetParams = {};
 
+        if(typeof reqParams.topic === 'undefined' || reqParams.topic === "" ){
+            res.status(200).send({error: 'Topic is missing!!!'});
+            return;
+        }
 
-        /*controller.getQuestions({
+        if(typeof reqParams.count === 'undefined' || reqParams.count === "" ){
+            res.status(200).send({error: 'You need to provide count in order to fetch data.'});
+            return;
+        }
 
-            } ,function (err, result) {
+        questionSetParams = {
+            topic : reqParams.topic,
+            lod : reqParams.lod,
+            count : reqParams.count
+        };
+
+        controller.getQuestions(questionSetParams ,function (err, result) {
             if (err) {
-                console.error('Error in GET of vendros, ERROR::', err);
-                res.status(400).send({error: 'Something went wrong, please try later..!'});
+                console.error('Error in getQuestions function of questions, and here is the error \n', err);
+                res.status(400).send({error: 'Something went wrong!!!'});
                 return;
             }
             res.send(result);
             return;
-        })*/
+        });
     } catch (err) {
         console.error('Unexpected error in GET of vendors, ERROR::', err);
         res.status(500).send({error: 'Unexpected internal error, please try later..!'});

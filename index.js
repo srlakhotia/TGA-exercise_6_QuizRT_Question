ae50fe89f08c4802d408a0fc06bdbac50fc6c418
Sneh Raj Lakhotia <srlakhotia@gmail.com>
2018-01-17 13:13:48 +0530

    commenting api including bodyparser

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let app = express();
 // Setup bodyParsing middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(require('./api'));
+// app.use(require('./api'));
 app.listen(3000, () => console.log('App listening on port 3000!'));
 
 module.exports = app;

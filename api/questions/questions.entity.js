eb5cb4acb11126f90f21e933a259fd842ce91865
Sneh Raj Lakhotia <srlakhotia@gmail.com>
2018-01-17 13:38:55 +0530

    question entity

---
+++ api/questions/questions.entity.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const lod = ["easy", "medium", "hard"];
+
+let schema = new mongoose.Schema({
+    label: { type: String, required: true },
+    image: { type: String, default: null },
+    topics: [ {type: String} ],
+    options: [{
+        _id: { type: Number, required: true },
+        text: { type: String, required: true }
+    }],
+    correctOption: Number,
+    lod: { type: String, required: true, enum: lod, default: "medium" },
+    analytics: {
+        ansTime: { type: Number, required: true },
+        correctness: { type: Number, required: true, min: 0, max: 100, default: 100 },
+        askedCount: { type: Number, required: true, default: 0 },
+        lastAsked: { type: Date, default: null }
+    }
+}, { collection: 'question' });
+
+
+module.exports = mongoose.model('question', schema);

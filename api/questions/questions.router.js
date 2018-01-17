f89189d1d19e5f2e3767857c25b953a4a86df1eb
pghose <newton@newtonflash.com>
2018-01-17 13:29:57 +0530

    added nodemon start service

---
+++ api/questions/questions.router.js
@@ -0,0 +1,25 @@
+const router = require('routers');
+const controller = require('./questions.router');
+
+// topic, level of difficulty, number of questions
+router.get('/', function(req, res){
+    try {
+        let reqParams = req;
+        console.log(reqParams);
+        /*controller.getQuestions({
+
+            } ,function (err, result) {
+            if (err) {
+                console.error('Error in GET of vendros, ERROR::', err);
+                res.status(400).send({error: 'Something went wrong, please try later..!'});
+                return;
+            }
+            res.send(result);
+            return;
+        })*/
+    } catch (err) {
+       /* console.error('Unexpected error in GET of vendors, ERROR::', err);
+        res.status(500).send({error: 'Unexpected internal error, please try later..!'});*/
+        return;
+    }
+});

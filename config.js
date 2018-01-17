d1397896b35ce8a971420c250806b4e6156845ed
Sneh Raj Lakhotia <srlakhotia@gmail.com>
2018-01-17 13:18:20 +0530

    updating db name

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {
 	MONGO: {
-		MONGO_URL: (process.env.MONGO_URL || 'mongodb://localhost:27017/questions')
+		MONGO_URL: (process.env.MONGO_URL || 'mongodb://localhost:27017/questiondb')
 	}
 };
 

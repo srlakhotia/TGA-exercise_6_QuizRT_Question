109c42fd29c2bb1fbef22876538e4b5a54a5c6aa
pghose <newton@newtonflash.com>
2018-01-22 16:14:07 +0530

    Added react views

---
+++ views/add-question/add-stub-form.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+
+class addStubForm extends React.Component {
+    render() {
+        return (
+            <form>
+                <div>
+                    <label>Add question stub</label>
+                </div>
+
+            </form>
+            )
+
+
+    }
+}
+
+module.exports = addStubForm;
